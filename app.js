fc6dbf1ad572729257dd209ee8be099ef112bfd7
DavidS415 <93840316+DavidS415@users.noreply.github.com>
2022-07-09 13:03:47 -0700

    update MVC (controller)

--- app.js
+++ app.js
@@ -8,40 +8,11 @@ app.set("view engine", "ejs");
 const dotenv = require("dotenv");
 dotenv.config();
 var mongoose = require("mongoose");
-const Result = require("./models/Result");
-
 const database = process.env.MONGOLAB_URI;
 mongoose.Promise = global.Promise;
 mongoose.connect(database);
 
-app.get("/", function (req, res) {
-    res.render("index",{ details: null });
-})
-
-app.get("/form", function (req, res) {
-    res.render("form");
-})
-
-app.get("/getdetails", function (req, res) {   
-    Result.find({}, function (err, allDetails) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("index", { details: allDetails })
-        }
-    })
-})
-
-app.post("/submit", (req, res) => {
-    var myData = new Result(req.body);
-    myData.save()
-        .then(item => {
-            res.redirect("/getdetails")
-        })
-        .catch(err => {
-            res.status(400).send("Unable to save to database");
-        });
-});
+app.use('/', require('./controllers/survey'));
 
 app.listen(port, () => {
     console.log("Server listening on port " + port);
---
+++ controllers/survey.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const Result = require("../models/Result");
+
+router.get("/", function (req, res) {
+    res.render("index",{ details: null });
+})
+
+router.get("/form", function (req, res) {
+    res.render("form");
+})
+
+router.get("/getdetails", function (req, res) {   
+    Result.find({}, function (err, allDetails) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", { details: allDetails })
+        }
+    })
+})
+
+router.post("/submit", (req, res) => {
+    var myData = new Result(req.body);
+    myData.save()
+        .then(item => {
+            res.redirect("/getdetails")
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+module.exports = router;
